cac165ab23cc5d8f6a07c0e7231375af9e23be99
12999422 <12999422@itisrossi.vi.it>
2026-01-31 14:24:52 +0100

    Revert "change layout.tsx"
    This reverts commit b83adeb1248f7d8266fc879ef763b11f42f49112.

--- app/(student)/providers.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { CartProvider } from "@/app/context/CartContext"; // uguale agli altri file
-
-export default function StudentProviders({ children }: { children: React.ReactNode }) {
-  return <CartProvider>{children}</CartProvider>;
-}
