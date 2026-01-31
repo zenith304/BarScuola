79fc43de5a9458a4eaed314b5ce091f3290ce868
12999422 <12999422@itisrossi.vi.it>
2026-01-31 15:00:12 +0100

    Revert "change layout.tsx"
    This reverts commit b83adeb1248f7d8266fc879ef763b11f42f49112.

--- app/(student)/layout.tsx
+++ app/(student)/layout.tsx
@@ -1,20 +1,19 @@
 import { Navbar } from '@/app/components/Navbar';
 import { CartProvider } from '@/app/context/CartContext';
-import StudentProviders from "./providers";
 
 export default function StudentLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <StudentProviders>
-      <div className="min-h-screen bg-gray-50 pb-20">
-        <Navbar type="student" />
-        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-          {children}
-        </main>
-      </div>
-    </StudentProviders>
-  );
+    return (
+        <CartProvider>
+            <div className="min-h-screen bg-gray-50 pb-20">
+                <Navbar type="student" />
+                <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+                    {children}
+                </main>
+            </div>
+        </CartProvider>
+    );
 }
